d679687b892fb560b130cf02f299233f91e8da05
preethi1210 <156685280+preethi1210@users.noreply.github.com>
2024-08-11 20:09:25 +0530

    Delete to_do_app directory

--- to_do_app/routes/Todo.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createTodo } = require("../controllers/createTodo");
-
-router.post("/createTodo", createTodo);
-
-module.exports = router;
